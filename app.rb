d7882b03e419bfa0daac4f75fbb3759f14299e61
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-04-26 15:39:39 +0100

    Adds attack page and message

--- app.rb
+++ app.rb
@@ -18,6 +18,16 @@ class Battle < Sinatra::Application
   get'/play' do
     @player1 = session[:player1]
     @player2 = session[:player2]
+    @player1_hp = 100
+    @player2_hp = 100
+    session[:player1_hp]= @player1_hp
+    session[:player2_hp]= @player2_hp
     erb(:play)
+  end
+
+  get '/attack' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:attack)
   end
 end

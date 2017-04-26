0d3360dbeae994782bd329abcdf2e6db81c307f9
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-04-26 15:41:57 +0100

    Removed some unnecessary code in app.rb

--- app.rb
+++ app.rb
@@ -18,10 +18,6 @@ class Battle < Sinatra::Application
   get'/play' do
     @player1 = session[:player1]
     @player2 = session[:player2]
-    @player1_hp = 100
-    @player2_hp = 100
-    session[:player1_hp]= @player1_hp
-    session[:player2_hp]= @player2_hp
     erb(:play)
   end
 
